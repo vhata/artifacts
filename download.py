0a8cbbd5c91913dda0aeb6640a9daea2c59270d8
Jonathan Hitchcock <jonathan.hitchcock@gmail.com>
2011-04-13 17:58:12 -0700

    Add timestamp to download.py

--- download.py
+++ download.py
@@ -9,4 +9,5 @@ d = artifacts.Downloader(config['bucket'], config['access_key'], config['secret_
 for f in args:
     vs = d.get_versions(f, config['product'], config.get('environment', None))
     for v in vs:
-        print "%s (%s)" % (v.name, v.get_metadata('version'))
+        print "%s (%s) at %s" % (v.name, v.get_metadata('version'),
+                v.last_modified)
